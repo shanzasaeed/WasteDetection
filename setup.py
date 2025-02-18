450a76bae75a53d8a621d7e5c25c887384bf457e
shanzasaeed <shanzasaeed2k@gmail.com>
2025-02-19 00:34:37 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='WasteClassification',
+    version='0.1',
+    author='Shanza Saeed',
+    author_email='shanzasaeed2k@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
